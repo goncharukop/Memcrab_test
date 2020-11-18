bed3cb5b424894f69f2500f53acf13e55c639058
goncharukop <goncharukop@gmail.com>
2020-11-18 22:59:12 +0200

    createMatrix MxN

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import './App.scss';
-import { Switch, Link, Route } from 'react-router-dom';
+// import { Switch, Link, Route } from 'react-router-dom';
 
-export const App = () => (
-  <div>
-    React starter pack
+export const App = () => {
+  const M = 5;
+  const N = 4;
+  const [state, setState] = useState([]);
+
+  const matrixCreate = () => {
+    const matrix = [];
+
+    for (let i = 0; i < M * N; i += 1) {
+      const ramdomNum = (Math.random() * 1000);
+
+      matrix.push({
+        id: i + 1,
+        Amount: Math.ceil(ramdomNum > 100 ? ramdomNum : ramdomNum + 100),
+      });
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(matrix);
+
+    return matrix;
+  };
+
+  return (
     <div>
-      <nav className="nav">
+      <table>
+        <tr>
+          <td className="table_field">The table body</td>
+          <td>with two columns</td>
+        </tr>
+      </table>
+
+      <button type="button" onClick={() => matrixCreate()}>CLIIIIIK</button>
+      {/* <nav className="nav">
         <Link to="/">Home</Link>
         <Link to="/users">Users</Link>
       </nav>
@@ -19,6 +49,7 @@ export const App = () => (
           <div>Home page</div>
         </Route>
       </Switch>
+    </div> */}
     </div>
-  </div>
-);
+  );
+};
