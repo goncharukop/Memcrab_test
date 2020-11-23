31796d805562a1f5884b30befd258ce5184489f9
goncharukop <goncharukop@gmail.com>
2020-11-23 23:05:07 +0200

    add  inputs

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -4,80 +4,62 @@
 import React, { useState } from 'react';
 import './Table.scss';
 
-let M = 5;
-const N = 6;
-
-const matrixCreate = () => {
-  const matrix = [];
-
-  for (let i = 0; i < M; i += 1) {
-    const matrixRow = [];
-
-    for (let j = 0; j < N; j += 1) {
-      const randomNum = Math.floor(Math.random() * 1000);
-
-      matrixRow.push({
-        id: [i, j],
-        amount: randomNum > 100 ? randomNum : randomNum + 100,
-        className: 'table__cell ui button',
-      });
-    }
-
-    matrix.push(matrixRow);
-  }
-
-  return matrix;
-};
-
-const getColumnAverage = (newMatrix) => {
-  const columnAverage = [];
-
-  for (let j = 0; j < N; j += 1) {
-    let temp = 0;
-
-    for (let i = 0; i < M; i += 1) {
-      if (newMatrix[i][j].id[1] === j) {
-        temp += matrix[i][j].amount;
-      }
-    }
-
-    columnAverage.push({
-      average: M ? Math.round(temp / M) : 0,
-      id: j,
-    });
-  }
-
-  return columnAverage;
-};
-
-const getRowSum = (newMatrix) => {
-  const rowSum = [];
-
-  for (let i = 0; i < M; i += 1) {
-    rowSum
-      .push({
-        sum: newMatrix[i].reduce((acc, el) => acc + el.amount, 0),
-        id: i,
-      });
-  }
-
-  return rowSum;
-};
-
-let matrix = matrixCreate();
 let showPercent = false;
+let showTable = false;
 
 export const Table = () => {
-  const [newMatrix, setNewMatrix] = useState(matrix);
-  const [X, setX] = useState(3);
+  const [M, setM] = useState(15);
+  const [N, setN] = useState(15);
+  const [X, setX] = useState(5);
+  const [error, setError] = useState('');
 
-  const rowSum = getRowSum(newMatrix);
-  const columnAverage = getColumnAverage(newMatrix);
+  const matrixCreate = () => {
+    const matrix = [];
+
+    for (let i = 0; i < M; i += 1) {
+      const matrixRow = [];
+
+      for (let j = 0; j < N; j += 1) {
+        const randomNum = Math.floor(Math.random() * 1000);
+
+        matrixRow.push({
+          id: [i, j],
+          amount: randomNum > 100 ? randomNum : randomNum + 100,
+          className: 'table__cell ui button',
+        });
+      }
+
+      matrix.push(matrixRow);
+    }
+
+    return matrix;
+  };
+
+  const [newMatrix, setNewMatrix] = useState(matrixCreate());
 
   const addOne = ({ el }) => {
-    el.amount += 1;
+    newMatrix
+      .forEach(row => row
+        .forEach((cell) => {
+          if (cell.id === el.id) {
+            el.amount += 1;
+          }
+        }));
 
-    setNewMatrix([...matrix, el.amount + 1]);
+    setNewMatrix([...newMatrix]);
+  };
+
+  const handleCreate = (event) => {
+    event.preventDefault();
+
+    if (!M || !N || !X) {
+      setError('Fill all fields, please');
+
+      return;
+    }
+
+    setNewMatrix(matrixCreate);
+    showTable = true;
   };
 
   const handleChange = (event) => {
@@ -87,10 +69,55 @@ export const Table = () => {
       case 'X':
         setX(value);
         break;
+      case 'N':
+        setN(value);
+        break;
+      case 'M':
+        setM(value);
+        break;
       default:
         break;
     }
   };
+
+  const getColumnAverage = () => {
+    const columnAverage = [];
+
+    for (let j = 0; j < N; j += 1) {
+      let temp = 0;
+
+      for (let i = 0; i < M; i += 1) {
+        if (newMatrix[i][j].id[1] === j) {
+          temp += newMatrix[i][j].amount;
+        }
+      }
+
+      columnAverage.push({
+        average: M ? Math.round(temp / M) : 0,
+        id: j,
+      });
+    }
+
+    return columnAverage;
+  };
+
+  const columnAverage = getColumnAverage();
+
+  const getRowSum = () => {
+    const rowSum = [];
+
+    for (let i = 0; i < M; i += 1) {
+      rowSum
+        .push({
+          sum: newMatrix[i].reduce((acc, el) => acc + el.amount, 0),
+          id: i,
+        });
+    }
+
+    return rowSum;
+  };
+
+  const rowSum = getRowSum(newMatrix);
 
   const addRow = () => {
     for (let i = M; i <= M; i += 1) {
@@ -106,29 +133,22 @@ export const Table = () => {
         });
       }
 
-      matrix.push(newMatrixRow);
+      newMatrix.push(newMatrixRow);
     }
 
-    M += 1;
-    setNewMatrix([...matrix]);
+    setM(M + 1);
+    setNewMatrix([...newMatrix]);
 
     return M;
   };
 
   const deleteRow = ({ row }) => {
-    for (const newRow of matrix) {
+    for (const newRow of newMatrix) {
       if (newRow[0].id[0] === row.id) {
-        matrix = [...matrix].filter(rowM => rowM !== newRow);
+        setNewMatrix(newMatrix.filter(el => el !== newRow));
+        setM(M - 1);
       }
     }
-
-    M -= 1;
-    setNewMatrix(matrix);
-
-    // eslint-disable-next-line no-console
-    console.log(matrix);
-
-    return M;
   };
 
   const getNeighborValue = ({ el }) => {
@@ -136,7 +156,7 @@ export const Table = () => {
     let temp = 0;
 
     while (counter < X) {
-      matrix.forEach((row) => {
+      newMatrix.forEach((row) => {
         row.forEach((cell) => {
           if (el.amount + temp >= cell.amount
           && el.amount - temp <= cell.amount
@@ -157,28 +177,23 @@ export const Table = () => {
       });
     }
 
-    setNewMatrix([...matrix]);
-
-    return matrix;
+    setNewMatrix([...newMatrix]);
   };
 
   const getInitialColor = () => {
-    matrix.forEach((row) => {
+    newMatrix.forEach((row) => {
       row.forEach((cell) => {
         cell.className = 'table__cell ui button';
       });
     });
 
-    setNewMatrix([...matrix]);
-
-    return matrix;
+    setNewMatrix([...newMatrix]);
   };
 
   const getPercent = ({ row }) => {
-    matrix.forEach((newRow) => {
+    newMatrix.forEach((newRow) => {
       newRow.forEach((cell) => {
         if (row.id === cell.id[0]) {
-          // eslint-disable-next-line no-restricted-properties
           cell.percent = `${(cell.amount / row.sum * 100).toFixed(2)}%`;
         } else {
           cell.percent = cell.amount;
@@ -187,129 +202,169 @@ export const Table = () => {
     });
 
     showPercent = true;
-
-    setNewMatrix([...matrix]);
-
-    return matrix;
+    setNewMatrix([...newMatrix]);
   };
 
   const getAmount = () => {
     showPercent = false;
 
-    setNewMatrix([...matrix]);
-
-    return matrix;
+    setNewMatrix([...newMatrix]);
   };
 
   return (
     <>
-      <label>
-        {`X : `}
-        <input
-          type="number"
-          value={X}
-          name="X"
-          onChange={handleChange}
-        />
-      </label>
-
-      <div className="table-major">
-        <table className="table">
-          <tbody>
-            <th>
-              Table with random values
-              { matrix.map(row => (
-                <tr key={row[0].id[0]}>
-                  {row.map(el => (
-                    <td key={el.id}>
-                      <button
-                        type="button"
-                        className={el.className}
-                        onClick={() => addOne({ el })}
-                        onMouseOver={() => {
-                          getInitialColor();
-                          getNeighborValue({ el });
-                        }}
-                        onFocus={() => getInitialColor()}
-                      >
-                        {
-                          showPercent
-                            ? `${el.percent}`
-                            : `${el.amount}`
-                        }
-                        <span
-                          className="table__cell-percent"
-                          style={{ height: showPercent ? `${el.percent}` : `` }}
-                        />
-                      </button>
-                    </td>
-                  ))}
-                </tr>
-              ))}
-            </th>
-          </tbody>
-        </table>
-
-        <table className="table">
-          <tbody>
-            <th>Sum</th>
-            { rowSum.map(row => (
-              <tr key={row.id}>
-                <td>
-                  <button
-                    type="button"
-                    className="table__cell ui button yellow"
-                    onMouseOver={() => {
-                      getInitialColor();
-                      getPercent({ row });
-                    }}
-                    onMouseOut={() => getAmount()}
-                    onFocus={() => getInitialColor()}
-                    onBlur={() => getAmount()}
-                  >
-                    {row.sum}
-                  </button>
-                </td>
-                <button
-                  type="button"
-                  className="ui button red"
-                  onClick={() => deleteRow({ row })}
-                >
-                  Delete row
-                </button>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <table className="table">
-        <tbody>
-          <th>
-            Average value in column
-            <tr>
-              { columnAverage.map(number => (
-                <td key={number.id}>
-                  <button
-                    type="button"
-                    className="table__cell ui button orange"
-                  >
-                    {Math.round(number.average)}
-                  </button>
-                </td>
-              ))}
-            </tr>
-          </th>
-        </tbody>
-      </table>
-
-      <button
-        type="button"
-        className="table__cell ui button green add-btn"
-        onClick={() => addRow()}
+      <form
+        className="ui form initial-form"
+        onSubmit={handleCreate}
       >
-        Add row
-      </button>
+        <label>
+          {`M : `}
+          <input
+            type="number"
+            value={M}
+            name="M"
+            placeholder="Enter columns"
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          {`N : `}
+          <input
+            type="number"
+            value={N}
+            name="N"
+            placeholder="Enter rows"
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          {`X : `}
+          <input
+            type="number"
+            value={X}
+            name="X"
+            onChange={handleChange}
+          />
+        </label>
+
+        <button
+          type="submit"
+          className="ui button green"
+        >
+          Create table
+        </button>
+        <p>{error}</p>
+      </form>
+
+      {
+        !showTable
+          ? <h3 className="create-text">Click create table, pease</h3>
+          : (
+            <>
+              <div className="table-major">
+                <table className="table">
+                  <tbody>
+                    <th>
+                      Table with random values
+                      { newMatrix.map(row => (
+                        <tr key={row[0].id[0]}>
+                          {row.map(el => (
+                            <td key={el.id}>
+                              <button
+                                type="button"
+                                className={el.className}
+                                onClick={() => addOne({ el })}
+                                onMouseOver={() => {
+                                  getInitialColor();
+                                  getNeighborValue({ el });
+                                }}
+                                onFocus={() => getInitialColor()}
+                              >
+                                {
+                                  showPercent
+                                    ? `${el.percent}`
+                                    : `${el.amount}`
+                                }
+                                <span
+                                  className="table__cell-percent"
+                                  style={{
+                                    height: showPercent
+                                      ? `${el.percent}`
+                                      : ``,
+                                  }}
+                                />
+                              </button>
+                            </td>
+                          ))}
+                        </tr>
+                      ))}
+                    </th>
+                  </tbody>
+                </table>
+
+                <table className="table">
+                  <tbody>
+                    <th>Sum</th>
+                    { rowSum.map(row => (
+                      <tr key={row.id}>
+                        <td>
+                          <button
+                            type="button"
+                            className="table__cell ui button yellow"
+                            onMouseOver={() => {
+                              getInitialColor();
+                              getPercent({ row });
+                            }}
+                            onMouseOut={() => getAmount()}
+                            onFocus={() => getInitialColor()}
+                            onBlur={() => getAmount()}
+                          >
+                            {row.sum}
+                          </button>
+                        </td>
+                        <button
+                          type="button"
+                          className="ui button red"
+                          onClick={() => deleteRow({ row })}
+                        >
+                          Delete row
+                        </button>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+
+              <table className="table">
+                <tbody>
+                  <th>
+                    Average value in column
+                    <tr>
+                      { columnAverage.map(number => (
+                        <td key={number.id}>
+                          <button
+                            type="button"
+                            className="table__cell ui button orange"
+                          >
+                            {Math.round(number.average)}
+                          </button>
+                        </td>
+                      ))}
+                    </tr>
+                  </th>
+                </tbody>
+              </table>
+
+              <button
+                type="button"
+                className="table__cell ui button green add-btn"
+                onClick={() => addRow()}
+              >
+                Add row
+              </button>
+            </>
+          )
+      }
     </>
   );
 };
